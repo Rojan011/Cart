549fe5dcef241c9315aaefdf78ac78775b9c9070
Rojan <shrestharojan79@gmail.com>
2025-03-15 20:24:02 +0545

    Store Item complete

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import { Trash2 } from "lucide-react";
 import { formatCurrency } from "../utils/formatCurrency";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 type StoreItemProps = {
   id: number;
@@ -9,7 +10,13 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-  const quantity = 1;
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+  const quantity = getItemQuantity(id);
 
   return (
     <div className="flex flex-col gap-3 bg-white p-4 rounded-lg shadow-md h-full">
@@ -29,24 +36,34 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
       {/* Add to Cart / Quantity Controls */}
       <div className="mt-auto w-full">
         {quantity === 0 ? (
-          <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
+          <button
+            className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+            onClick={() => increaseCartQuantity(id)}
+          >
             + Add to Cart
           </button>
         ) : (
           <div className="flex justify-between items-center w-full">
             {/* Quantity Controls */}
             <div className="flex items-center gap-3">
-              <button className="bg-blue-500 text-white w-8 h-8 flex items-center justify-center rounded hover:bg-blue-600 transition duration-100 cursor-pointer">
+              <button
+                onClick={() => decreaseCartQuantity(id)}
+                className="bg-blue-500 text-white w-8 h-8 flex items-center justify-center rounded hover:bg-blue-600 transition duration-100 cursor-pointer"
+              >
                 -
               </button>
               <span className="text-2xl">{quantity}</span>
-              <button className="bg-blue-500 text-white w-8 h-8 flex items-center justify-center rounded hover:bg-blue-600 transition duration-100 cursor-pointer">
+              <button
+                onClick={() => increaseCartQuantity(id)}
+                className="bg-blue-500 text-white w-8 h-8 flex items-center justify-center rounded hover:bg-blue-600 transition duration-100 cursor-pointer"
+              >
                 +
               </button>
             </div>
 
             {/* Remove Button */}
-            <button className="bg-red-500 text-white px-3 py-2 rounded hover:bg-red-600 transition duration-300 cursor-pointer">
+            <button
+            onClick={() => removeFromCart(id)} className="bg-red-500 text-white px-3 py-2 rounded hover:bg-red-600 transition duration-300 cursor-pointer">
               <Trash2 />
             </button>
           </div>

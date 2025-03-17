d11450dde73090ee6b248cc98ade12914360f729
Rojan <shrestharojan79@gmail.com>
2025-03-17 16:48:48 +0545

    fixes

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -4,8 +4,8 @@ import storeItems from "../data/items.json";
 export function Store() {
   return (
     <>
-      <div className="text-2xl mb-2">Stuffs you may like</div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+      <div className="text-2xl mb-6">Available Items</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-5">
         {storeItems.map((item) => (
           <div key={item.id}>
             <StoreItem {...item} />

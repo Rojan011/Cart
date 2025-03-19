405704479e0b6f76406cfecaa2732b2cbcb4a8b2
SansKc <sanskarkc51@gmail.com>
2025-03-19 17:46:46 +0545

    Hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import Carousel from "../components/Carousel";
 import Hero from "../components/Hero";
 
 export function Home() {
   return (
+    <>
     <Hero/>
+    <Carousel/>
+    </>
+    
   );
 }
